153333bf8d061e1f8a1828d67b950bc464b15114
lepeshkinff <lepeshkinff@gmail.com>
2020-05-10 14:56:43 +0300

    чтение конфигурации из файла

--- src/Kraken.Engine/ConfigurationsProvider.cs
+++ src/Kraken.Engine/ConfigurationsProvider.cs
@@ -1,29 +1,19 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 
 namespace Kraken.Engine
 {
-   public class ConfigurationsProvider
-   {
-      private string configurationPath;
+	public class ConfigurationsProvider
+	{
+		private string configurationPath;
 
-      public ConfigurationsProvider(string configurationPath)
-      {
-         this.configurationPath = configurationPath;
-      }
+		public ConfigurationsProvider(string configurationPath)
+		{
+			this.configurationPath = configurationPath;
+		}
 
-      //любители локального iis смогут доработать этот метод так, чтобы можно было писать локальные перегрузки и не коммитить свои правила в общйи гит
-      //а для меня норм и так
-      public IReadOnlyCollection<FileConfiguration> GetConfigurations()
-      {
-         var assembly = Assembly.GetExecutingAssembly();
-         using (Stream stream = assembly.GetManifestResourceStream(configurationPath))
-         using (StreamReader reader = new StreamReader(stream))
-         {
-            return JsonConvert.DeserializeObject<List<FileConfiguration>>(reader.ReadToEnd());
-         }
-      }
-   }
+		public IReadOnlyCollection<FileConfiguration> GetConfigurations() =>
+			JsonConvert.DeserializeObject<List<FileConfiguration>>(File.ReadAllText(configurationPath));
+	}
 }
